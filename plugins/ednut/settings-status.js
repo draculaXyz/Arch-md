8783cabef13e7463dc828c50ff216bcdb7969ef2
draculaXyz <plsworm6@gmail.com>
2025-07-04 21:17:09 +0100

    Create settings-status.js

---
+++ plugins/ednut/settings-status.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    command: ["autosend", "astatus"],
+    description: "Toggle auto status sender: on/off",
+    category: "Owner",
+    ban: true,
+    gcban: true,
+    owner: true,
+    execute: async (m, { prefix, isOwner, isGroup, args }) => {
+      if (isGroup && global.db.groups[m.chat]?.banned) return;
+      if (global.db.users[m.sender]?.banned) return m.reply("You are banned from using the bot.");
+      if (!isOwner) return m.reply("This command is for the owner only.");
+      if (!args[0]) return m.reply(`Example: ${prefix}autosend on/off`);
+
+      const option = args[0].toLowerCase();
+
+      switch (option) {
+        case "on":
+          if (global.db.settings.send) return m.reply("Autosend is already enabled.");
+          global.db.settings.send = true;
+          m.reply("Autosend turned *ON* successfully.");
+          break;
+
+        case "off":
+          if (!global.db.settings.send) return m.reply("Autosend is already disabled.");
+          global.db.settings.send = false;
+          m.reply("Autosend turned *OFF* successfully.");
+          break;
+
+        default:
+          m.reply("Invalid option! Use one of: on/off");
+      }
+    },
+  }
+];
